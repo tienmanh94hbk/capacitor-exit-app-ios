ffdbb69b63658a754f1db29b5b845b6621d1be5b
Do Tien Manh/ ドオ ティエン マイン <dotien.manh@bigtreetc.com>
2021-11-02 12:48:47 +0900

    Added TypeScript support; Updated iOS target to 12.0

--- src/definitions.ts
+++
@@ -1,9 +0,0 @@
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    ExitAppIosPlugin: ExitAppIosPluginPlugin;
-  }
-}
-
-export interface ExitAppIosPluginPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
-}
